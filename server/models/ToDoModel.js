cfca72e2bc5c305111d86270443d725c9e4eb935
Ashutosh Mishra <ashutoshmishra61679@gmail.com>
2024-05-22 01:42:54 +0530

    add ToDoModel

---
+++ server/models/ToDoModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    }, 
+    dueDate: {
+        type: Date, 
+        required: true
+    },
+    hoursNeeded: {
+        type: Number,
+        required: false
+    }
+}, {timestamps: true})
+
+
+module.exports = mongoose.model("Todo", TodoSchema);
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,5 +1,6 @@
  const express = require("express");
  const router = express.Router();
+ const TodoModel = require("../models/ToDoModel")
 
  router.get('/', (req, res)=>{
     res.json({
@@ -13,10 +14,15 @@
     })
  })
 
- router.post('/', (req, res)=>{
-    res.json({
-        msg: "POST a new todo"
-    })
+ router.post('/', async (req, res)=>{
+    const {title, dueDate, hoursNeeded} = req.body;
+    try {
+        const todo = await TodoModel.create({title, dueDate, hoursNeeded});
+        res.status(200).json(todo)
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
  })
 
  router.delete('/:id', (req, res)=>{
