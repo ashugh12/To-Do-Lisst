cff1499a936a7da3905e942204bd9a1b22d3beee
Ashutosh Mishra <ashutoshmishra61679@gmail.com>
2024-05-22 01:26:58 +0530

    connected to db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose")
 const express = require('express');
 const app = express();
 const todoRouter = require('./routes/todo')
@@ -12,7 +13,10 @@ app.use((req, res, next)=>{
 
 app.use('/api/todos', todoRouter);
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`http://localhost:${process.env.PORT}`)
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`connected to db and http://localhost:${process.env.PORT} `)
+    })
+}).catch((error)=>{
+    console.log(error);
 })
