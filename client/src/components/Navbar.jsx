7ce53905b9e43df6aebd2d2a602986e87d52391e
Ashutosh Mishra <ashutoshmishra61679@gmail.com>
2024-05-22 17:04:27 +0530

    React app

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+
+const Navbar = ()=>{
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        <Link to="/api/todos">My Todo</Link>
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+
+export default Navbar;
+
