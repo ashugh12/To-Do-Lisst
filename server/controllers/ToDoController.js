f00372e8bd42a1259ac485b1e0c7b824e6e16003
Ashutosh Mishra <ashutoshmishra61679@gmail.com>
2024-05-22 16:10:25 +0530

    add toDo controller

---
+++ server/controllers/ToDoController.js
@@ -0,0 +1,52 @@
+const TodoModel =require("../models/ToDoModel");
+
+const getTodos = async (req, res) =>{
+    const todos = await TodoModel.find({}).sort({createdAt: -1});
+    res.status(200).json(todos);
+}
+
+const getTodo = async (req, res) =>{
+    const  {id} = req.params;
+    const todo = await TodoModel.findById(id);
+    if(!todo){
+        return res.status(404).json({error: "No such todo Found"})
+    }
+    return res.status(200).json(todo);
+}
+const createTodo = async (req, res)=>{
+        const {title, dueDate, hoursNeeded} = req.body;
+        try {
+            const todo = await TodoModel.create({title, dueDate, hoursNeeded});
+            res.status(200).json(todo)
+        }
+        catch(error){
+            res.status(400).json({error: error.message})
+        }
+    }
+
+const deleteTodo = async (req, res)=>{
+    const {id} = req.params;
+    const todo = await TodoModel.findOneAndDelete({_id: id});
+    if(!todo){
+        return res.status(404).json({error: "No such todo Found"})
+    }
+    return res.status(200).json(todo);
+}
+
+const updateTodo = async (req, res) =>{
+    const {id} = req.params;
+    const todo = await TodoModel.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+    if(!todo){
+        return res.status(404).json({error: "No such todo Found"})
+    }
+    return res.status(200).json(todo);
+}
+
+module.exports ={
+    getTodo,
+    getTodos,
+    createTodo,
+    updateTodo, deleteTodo,
+}
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,39 +1,13 @@
- const express = require("express");
- const router = express.Router();
- const TodoModel = require("../models/ToDoModel")
+const express = require("express");
+const router = express.Router();
+const { createTodo, getTodos, getTodo, deleteTodo, updateTodo } = require("../controllers/ToDoController");
+router.get("/", getTodos);
 
- router.get('/', (req, res)=>{
-    res.json({
-        msg: 'GET all todos'
-    })
- })
+router.get("/:id", getTodo);
 
- router.get('/:id', (req, res)=>{
-    res.json({
-        msg: "GET  a single todo"
-    })
- })
+router.post("/", createTodo);
 
- router.post('/', async (req, res)=>{
-    const {title, dueDate, hoursNeeded} = req.body;
-    try {
-        const todo = await TodoModel.create({title, dueDate, hoursNeeded});
-        res.status(200).json(todo)
-    }
-    catch(error){
-        res.status(400).json({error: error.message})
-    }
- })
+router.delete("/:id", deleteTodo)
 
- router.delete('/:id', (req, res)=>{
-    res.json({
-        msg:"DELETE a todo"
-    })
- })
-
- router.patch('/:id',( req, res)=>{
-    res.json({
-        msg: "UPDATE a todo"
-    })
- })
- module.exports = router;
+router.patch("/:id", updateTodo );
+module.exports = router;
